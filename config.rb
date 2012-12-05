246e89cad32550065a884a6230cb1bf8d4aee8db
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2012-12-05 13:58:05 +0400

    Added Compass stuff

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+#require 'zen-grids'
+#require 'breakpoint'
+require 'susy'
+require 'toolkit'
+
+# Set this to the root of your project when deployed:
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+#fonts_dir = "fonts"
+#javascripts_dir = "js"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+sass_options = {:debug_info => true}
